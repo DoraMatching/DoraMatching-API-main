d4c07aac6ec246755fc910ac02d1d237cf1a4d4f
tranphuquy19 <tranphuquy19@gmail.com>
2020-10-18 18:29:22 +0700

    test 3

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -7,16 +7,6 @@ docker exec 501 bash -c "psql -U postgres -c 'DROP DATABASE IF EXISTS doramatchi
 echo 'Creating the database...'
 docker exec 501 bash -c "psql -U postgres -c 'CREATE DATABASE doramatching;'"
 
-echo 'Checkout working directory'
-cd /home/pi/Documents/Test/DoraMatching-API-main
-git checkout dev
-
-echo 'Pulling newest code...'
-git pull
-
-echo 'Updating yarn...'
-sudo npm i -g yarn
-
 echo 'Installing dependencies...'
 yarn install
 
