33a1f426b6706aaaba690bc884fecd2662df1df1
christian <christian@enclave.vn>
2020-10-19 15:25:26 +0700

    edit swagger

--- src/modules/tag-question/tag-question.controller.ts
+++ src/modules/tag-question/tag-question.controller.ts
@@ -8,17 +8,17 @@ import { TagQuestionRO } from '@/modules/tag-question/dto';
 import { TagQuestionService } from '@/modules/tag-question/tag-question.service';
 import { apiUrl } from '@/config';
 
-@ApiTags('tag-post')
+@ApiTags('tag-question')
 @Controller()
 export class TagQuestionController {
     constructor(private readonly tagQuestionService: TagQuestionService) {
     }
 
     @Auth()
-    @ApiOperation({ summary: 'Get all tag-posts', description: 'Return 1 page of tag-post' })
+    @ApiOperation({ summary: 'Get all tag-questions', description: 'Return 1 page of tag-question' })
     @ApiResponse({ type: [TagQuestionRO], status: 200 })
     @Get('tag-post')
     getAllTags(@Query() pagOpts: PaginateParams, @User() jwtUser: JwtUser) {
-        return this.tagQuestionService.getAllTags({ ...pagOpts, route: `${apiUrl}/tag-post` }, jwtUser);
+        return this.tagQuestionService.getAllTags({ ...pagOpts, route: `${apiUrl}/tag-question` }, jwtUser);
     }
 }
