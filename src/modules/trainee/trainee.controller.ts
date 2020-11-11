6f4b8623c88a85986b51e0fe2ae29e48849976fb
tranphuquy19 <tranphuquy19@gmail.com>
2020-11-11 14:51:50 +0700

    add getTraineeByUserId

--- src/modules/trainee/trainee.controller.ts
+++ src/modules/trainee/trainee.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { TraineeService } from '@trainee/trainee.service';
-import { JwtUser } from '@user/dto';
+import { JwtUser, UserRO } from '@user/dto';
 import { User } from '@user/user.decorator';
 import { Auth } from '@/shared/auth';
-import { PaginateParams } from '@/shared';
+import { FindOneParams, PaginateParams } from '@/shared';
 import { apiUrl } from '@/config';
+import { UserQuery } from '@user/user.query';
+import { TraineeRO } from '@trainee/dto';
 
 @ApiTags('trainee')
 @Controller()
@@ -16,8 +18,26 @@ export class TraineeController {
     }
 
     @Auth()
+    @ApiOperation({ summary: 'Get all trainees', description: 'Return 1 one page of trainees' })
+    @ApiResponse({ type: TraineeRO, status: 200 })
     @Get('trainees')
     getAllTrainees(@Query() pagOpts: PaginateParams, @User() jwtUser: JwtUser) {
         return this.traineeService.getAllTrainees({ ...pagOpts, route: `${apiUrl}/trainees` }, jwtUser);
+    }
+
+    @Auth()
+    @ApiOperation({ summary: 'Get trainee by id', description: 'Return a trainee with :id' })
+    @ApiResponse({ type: TraineeRO, status: 200 })
+    @Get('trainee/:id')
+    getTraineeById(@Query() { id }: FindOneParams, @User() jwtUser: JwtUser) {
+        return this.traineeService.getTraineeById(id, jwtUser);
+    }
+
+    @Auth()
+    @ApiOperation({ summary: 'Get trainee by userId', description: 'Return a trainee with :userId' })
+    @ApiResponse({ type: TraineeRO, status: 200 })
+    @Get('trainee')
+    getTraineeByUserId(@Query() { userId }: UserQuery, @User() jwtUser: JwtUser) {
+        return this.traineeService.getTraineeByUserId(userId, jwtUser);
     }
 }
--- src/modules/trainee/trainee.service.ts
+++ src/modules/trainee/trainee.service.ts
@@ -20,6 +20,24 @@ export class TraineeService extends BaseService<TraineeEntity, TraineeRepository
         super(traineeRepository);
     }
 
+    async getTraineeByUserId(userId: string, jwtUser) {
+        const trainee = await this.traineeRepository.getTraineeByUserId(userId);
+        if (!trainee) throw new HttpException(`OOPS! Trainee with id: ${userId} not found!`, HttpStatus.NOT_FOUND);
+        const permission = grantPermission(this.rolesBuilder, AppResources.TRAINEE, 'read', jwtUser, trainee.user.id);
+        if (permission.granted) {
+            return permission.filter(trainee);
+        } else throw new HttpException(`You don't have permission for this!`, HttpStatus.FORBIDDEN);
+    }
+
+    async getTraineeById(id: string, jwtUser: JwtUser) {
+        const trainee = await this.traineeRepository.getTraineeById(id);
+        if (!trainee) throw new HttpException(`OOPS! Trainee with id: ${id} not found!`, HttpStatus.NOT_FOUND);
+        const permission = grantPermission(this.rolesBuilder, AppResources.TRAINEE, 'read', jwtUser, trainee.user.id);
+        if (permission.granted) {
+            return permission.filter(trainee);
+        } else throw new HttpException(`You don't have permission for this!`, HttpStatus.FORBIDDEN);
+    }
+
     async getAllTrainees(pagOpts: PaginateParams, jwtUser: JwtUser): Promise<IPagination<ITraineeRO>> {
         const permission = grantPermission(this.rolesBuilder, AppResources.TRAINEE, 'read', jwtUser, null);
         if (permission.granted) {
@@ -27,7 +45,7 @@ export class TraineeService extends BaseService<TraineeEntity, TraineeRepository
                 const data = await this.traineeRepository.getAllTrainees(pagOpts);
                 const result = customPaginate<TraineeRO>(data, pagOpts);
                 return paginateFilter(result, permission);
-            }catch ({ detail }) {
+            } catch ({ detail }) {
                 throw new HttpException(detail || 'OOPS!', HttpStatus.INTERNAL_SERVER_ERROR);
             }
 
