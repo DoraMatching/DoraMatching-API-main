a8162bb0b9d6770f6d8e2095580e47a89b6cd5eb
tranphuquy19 <tranphuquy19@gmail.com>
2021-03-21 09:49:23 +0700

    temp meeting service

--- src/modules/meeting/meeting.module.ts
+++ src/modules/meeting/meeting.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from '@user/repositories';
+import { TrainerRepository } from '@trainer/repositories';
 import { MeetingController } from './meeting.controller';
 import { MeetingService } from './meeting.service';
+import { MeetingRepository } from './repositories';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserRepository])],
+    imports: [TypeOrmModule.forFeature([UserRepository, MeetingRepository, TrainerRepository])],
     controllers: [MeetingController],
     providers: [MeetingService],
 })
--- src/modules/meeting/meeting.service.ts
+++ src/modules/meeting/meeting.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { TrainerRepository } from '@trainer/repositories';
 import { JwtUser } from '@user/dto';
 import { CreateMeetingDTO } from './dto';
+import { MeetingRepository } from './repositories';
 
 @Injectable()
 export class MeetingService {
-    async createMeeting(data: CreateMeetingDTO, jwtUser: JwtUser) {}
+    constructor(private readonly meetingRepository: MeetingRepository, private readonly trainerRepository: TrainerRepository) { }
+
+    async createMeeting(data: CreateMeetingDTO, jwtUser: JwtUser) {
+        const [trainer] = await Promise.all([this.trainerRepository.getTrainerByUserId(jwtUser.id)]);
+        if (!trainer) throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+        return { data };
+    }
 }
